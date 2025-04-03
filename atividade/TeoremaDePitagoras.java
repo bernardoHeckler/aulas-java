b537b310fc6ae1e549af85ee93729a4472cb4c0e
Bernardo Antunes Heckler <bernaheckler1@gmail.com>
2025-04-03 09:43:41 -0300

    fix: melhorando as frases

--- atividade/TeoremaDePitagoras.java
+++ atividade/TeoremaDePitagoras.java
@@ -1,13 +1,18 @@
+// RA: 1137118
+// Nome: Bernardo Antunes Heckler
+// Atividade: Cálculo da Hipotenusa com o Teorema de Pitágoras
+
 import java.util.Scanner;
 
 public class TeoremaDePitagoras {
     public static void main(String[] args) {
         Scanner lerTeclado = new Scanner(System.in);
-        System.out.println("Digite o valor de A: ");
+        System.out.println("\nDigite o valor de A: ");
         double a = lerTeclado.nextDouble();
         System.out.println("Digite o valor de B: ");
         double b = lerTeclado.nextDouble();
         double h = Math.sqrt(a*a + b*b);
-        System.out.println("Você acabou de Calcular \n"+ " Cateto a: "+ a*a + " Cateto B: "+ b*b +" Total de H é: " + h);
+        System.out.println("Você acabou de Calcular a Hipotenusa de um Triângulo Retângulo usando o Teorema de Pitágoras.\n"+ "Cateto A: "+ a +"²" + "\nCateto B: "+ b +"²");
+        System.out.printf("\nTotal de H é: %.2f", h);
     }    
 }
